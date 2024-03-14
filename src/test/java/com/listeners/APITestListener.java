bd0881e1c8a45ee8da556355d489186e896d285d
Amar <Amar@gmail.com>
2024-03-14 21:24:11 +0530

    Generated Report

--- src/test/java/com/listeners/APITestListener.java
+++ src/test/java/com/listeners/APITestListener.java
@@ -6,16 +6,30 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class APITestListener implements ITestListener {
+
+	private ExtentReports extentReports;
+	private ExtentTest extentTest;
+	private ExtentSparkReporter extentSparkReporter;
 
 	public void onTestStart(ITestResult result) { // ITestResult information about the test method? Desc, name, group
 		// TODO Auto-generated method stub
-
+		extentTest = extentReports.createTest(result.getName());
 		System.out.println("*********************************" + result.getName()
 				+ "Test Started ********************************* ");
 		System.out.println("DESCRIPTION:" + result.getMethod().getDescription());
 		System.out.println("GROUPS:" + Arrays.toString(result.getMethod().getGroups()));
 		System.out.println("START TIME" + result.getStartMillis());
+		extentTest.log(Status.INFO, "DESCRIPTION:" + result.getMethod().getDescription());
+		extentTest.log(Status.INFO, "GROUPS:" + Arrays.toString(result.getMethod().getGroups()));
 
 	}
 
@@ -24,6 +38,7 @@ public class APITestListener implements ITestListener {
 		System.out.println("*********************************" + result.getName()
 				+ " Test Passed!! ********************************* ");
 		System.out.println("End TIME" + result.getEndMillis());
+		extentTest.pass("Test passed!!");
 
 	}
 
@@ -32,6 +47,9 @@ public class APITestListener implements ITestListener {
 		System.out.println("*********************************" + result.getName()
 				+ " Test Failed!! ********************************* ");
 		System.out.println("End TIME" + result.getEndMillis());
+		Markup m = MarkupHelper.createCodeBlock(result.getThrowable().toString());
+		extentTest.fail(m);
+
 	}
 
 	public void onTestSkipped(ITestResult result) {
@@ -39,19 +57,23 @@ public class APITestListener implements ITestListener {
 		System.out.println("*********************************" + result.getName()
 				+ " Test Skipped!!! ********************************* ");
 		System.out.println("End TIME" + result.getEndMillis());
+		extentTest.skip("Test Skipped!!!");
 	}
 
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
 		System.out.println("********************************* TestSuite Started********************************* ");
-
+		extentSparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "//reports//report.html");
+		extentReports = new ExtentReports();
+		extentReports.attachReporter(extentSparkReporter);
 	}
 
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
 		System.out.println(
-				"********************************* TestSuite Completed!!!!!!********************************* ");
 
+				"********************************* TestSuite Completed!!!!!!********************************* ");
+		extentReports.flush();
 	}
 
 }
