35beb51d3573463db0e5ad2350f31124663798b8
jatin <=jatinvsharma@gmai.com>
2024-04-29 21:50:55 +0530

    Day 3

---
+++ src/main/java/com/selenium/day1/DashboardPage.java
@@ -0,0 +1,26 @@
+package com.selenium.day1;
+
+import com.utility.BrowserUtil;
+import static com.selenium.day1.PhoenixLocator.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DashboardPage extends BrowserUtil {
+
+	private static final By USER_ICON_BUTTON_LOCATOR = locateByXPATH("//mat-icon[@data-mat-icon-name=\"user-circle\"]");
+	private static final By USER_NAME_LOCATOR = locateByXPATH("//span[contains(text(), \"Signed in as\")]/../span[2]");
+
+	
+	public DashboardPage(WebDriver wd) {
+		// TODO Auto-generated constructor stub
+		super(wd);
+	}
+	public String getUserName() {
+		// TestStep
+		sleepFor(5);
+		clickOn(USER_ICON_BUTTON_LOCATOR);
+		String data = getVisibileText(USER_NAME_LOCATOR);
+		return data;
+	}
+}
--- src/main/java/com/utility/BrowserUtil.java
+++ src/main/java/com/utility/BrowserUtil.java
@@ -1,10 +1,12 @@
 package com.utility;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class BrowserUtil {
+public abstract class BrowserUtil {
 	/*
 	 * A very special class which will holding all the Selenium WebDriver methods It
 	 * will created a way that my team members or Non tech person can understand it
@@ -12,9 +14,19 @@ public class BrowserUtil {
 	 * DDD : Domain Driven Design
 	 * 
 	 */
-	private WebDriver wd;// webdriver cannot be static!!
+	protected WebDriver wd;// webdriver cannot be static!!
 	// Then you cannot do parallel testing
 	//
+
+	public BrowserUtil(WebDriver wd) {
+		super();
+		this.wd = wd; //assigning the session!! [LoginPage ------> Other page Classes
+	}
+	
+	public BrowserUtil(){
+		
+	}
+	
 
 	public void launchBrowser(Browser browser) {
 		if (browser == Browser.CHROME) {
@@ -26,13 +38,46 @@ public class BrowserUtil {
 
 		}
 	}
-	
+
 	public void goToWebSite(String url) {
 		wd.get(url);
+		sleepFor(5);
 	}
 
-	
 	public void maximizeTheWindow() {
 		wd.manage().window().maximize();
 	}
+
+	public void enterTextInto(By locator, String textToEnter) {
+		WebElement element = wd.findElement(locator);
+		element.clear();
+		element.sendKeys(textToEnter);
+	}
+
+	public void clickOn(By locator) {
+		WebElement element = wd.findElement(locator);
+		element.click();
+	}
+
+	public void sleepFor(int secs) {
+		try {
+			Thread.sleep(secs * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getVisibileText(By locator) {
+		WebElement element = wd.findElement(locator);
+		String data = element.getText();
+		return data;
+
+	}
+
+	public WebDriver getWd() {
+		return wd;
+	}
+
 }
